59e3036d62788bcd1b0c2e9b7617aeab50b3a428
mohamed alaa <mohalaa1520@gmail.com>
2025-06-03 11:44:44 +0300

    routing

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './../Navbar/Navbar';
+import Footer from './../Footer/Footer';
+
+function Layout() {
+  return <>
+  
+  <Navbar />
+  <Footer />
+  
+  </>
+    
+  
+}
+
+export default Layout
