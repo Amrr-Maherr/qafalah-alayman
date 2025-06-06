98dd9f2d036ef65d4a965ec1a578889ef5c67c5f
Amrr-Maherr <amrr.maherr242gmail.com>
2025-06-06 09:05:00 +0300

    - Integrated Framer Motion into the HomeBox component - Applied a smooth scale effect on hover using whileHover={{ scale: 1.05 }} - Removed incorrect import and replaced onHoverStart with proper Framer Motion syntax - Improved user interactivity with subtle animation feedback

--- src/components/Home/HomeBox.jsx
+++ src/components/Home/HomeBox.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
-
+import { motion, scale } from "framer-motion";
 export default function HomeBox({item,index}) {
     return (
       <>
         <Link to={item.link}>
-          <div
+          <motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.5 }}
+            transition={{ duration: 0.3 }}
             key={index}
             className="w-full max-w-[247px] h-auto mx-auto text-center flex items-center justify-center flex-col gap-4 relative z-50 
-               hover:bg-[#FBF5EA5C] rounded-[20px] transition duration-300 p-4 md:p-6"
+               hover:bg-[#FBF5EA5C] rounded-[20px]  p-4 md:p-6"
           >
             <img
               src={item.icon}
@@ -17,7 +20,7 @@ export default function HomeBox({item,index}) {
             <h3 className="text-xl md:text-2xl font-[700] text-[#FBF5EA]">
               {item.title}
             </h3>
-          </div>
+          </motion.div>
         </Link>
       </>
     );
