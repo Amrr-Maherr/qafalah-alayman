5e4668212182ee35227d7923f8579b579e4767fb
Amrr-Maherr <amrr.maherr242gmail.com>
2025-06-06 09:43:18 +0300

    - Created HeroSection component with customizable background, title, description, and button - Applied responsive Tailwind classes for heading, text, and button sizes - Used flex and text-center utilities for vertical and horizontal alignment - Enhanced layout with padding and max-width constraints for better readability on all devices

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,26 @@
 import Image from "../../assets/293621.jpg"
+import HeroSection from "../Herocomponents/HeroSection";
 import HomeNav from './HomeNav';
 import HomeOverLay from "./HomeOverLay"
 function Home() {
   return (
-    <section
-      className="min-h-screen relative"
-      style={{
-        backgroundImage: `url(${Image})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      {/* home title */}
-      <HomeNav/>
-      {/* home links */}
+    <>
+      <section
+        className="min-h-screen relative"
+        style={{
+          backgroundImage: `url(${Image})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* home title */}
+        <HomeNav />
+        {/* home links */}
 
-      <HomeOverLay/>
-    </section>
+        <HomeOverLay />
+      </section>
+    </>
   );
 }
 
