cca05c791a9feb0cbd0fabb07dede1debaec70c4
Amrr-Maherr <amrr.maherr242gmail.com>
2025-06-09 12:56:34 +0300

    install npm install swiper

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,23 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+export default function Slider({ children }) {
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        {children}
+      </Swiper>
+    </>
+  );
+}
