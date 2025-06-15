530261ba90cd0d59546d8adcc36f1dc32b50a586
Amrr-Maherr <amrr.maherr242gmail.com>
2025-06-15 17:46:13 +0300

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,31 @@
-import Image from "../../assets/home-image.png"
 import HeroSection from "../Herocomponents/HeroSection";
 import NewsTicker from "../NewsTicker";
-import HomeNav from './HomeNav';
-import HomeOverLay from "./HomeOverLay"
+import HomeNav from "./HomeNav";
+import HomeOverLay from "./HomeOverLay";
+
+import backgroundVideo from "../../assets/Generated File June 13, 2025 - 8_22PM.mp4";
+
 function Home() {
   return (
     <>
-      <section
-        className="min-h-screen relative"
-        style={{
-          backgroundImage: `url(${Image})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        {/* home title */}
-        <HomeNav />
-        {/* home links */}
+      <section className="min-h-screen relative overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+        >
+          <source src={backgroundVideo} type="video/mp4" />
+          المتصفح لا يدعم تشغيل الفيديو.
+        </video>
 
+        {/* محتوى الصفحة فوق الفيديو */}
+        <HomeNav />
         <HomeOverLay />
       </section>
     </>
   );
 }
 
-export default Home
+export default Home;
